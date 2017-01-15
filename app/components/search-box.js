214d7156f050d050f2962fec0a122f05bea3bacc
Sebastien ASTORI <Sebastien ASTORI>
2017-01-15 16:17:56 +0100

    add search-box et un peu sass

---
+++ app/components/search-box.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    className:'row',
+    lieu:null,
+    metier:null,
+    proche:null
+});
--- app/serializers/adresse.js
+++ app/serializers/adresse.js
@@ -3,13 +3,13 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     let data = [];
-
+      id=null;
       payload.features.forEach((adresse)=>{
           adresse.properties.lat=adresse.geometry.coordinates[0];
           adresse.properties.long=adresse.geometry.coordinates[1];
           adresse.attributes= adresse.properties;
           adresse.type='adresse';
-          adresse.id=adresse.attributes.id;
+          adresse.id=adresse.geometry.coordinates.toString();
           data.push(adresse);
  
       });
